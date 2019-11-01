770850f4a50f2472c9a7ca6f879f4d76c246bbac
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-11-02 00:22:29 +0100

    Hello, Vue!

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="wrapper">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data() {
+      return {
+        message: 'Hello, Vue!'
+      }
+    },
+    computed: {
+    },
+    methods: {
+    },
+    mounted() {
+    }
+  }
+
+</script>
