d72028f698ca0de7e3bec8cdca28f724f8b7a2b3
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-11-02 15:44:25 +0100

    Initial implementation of Progressbar component

---
+++ src/components/Progressbar.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  .progressbar
+    .inner(v-bind:style="{ width: progress + '%' }")
+      p {{ progress }}% completed
+</template>
+
+<script>
+  export default {
+    name: 'progressbar',
+    data() {
+      return {
+        progress: 30,
+      }
+    },
+    methods: {
+      setRandom() {
+        let self = this
+        let random = Math.floor(Math.random() * Math.floor(100))
+        this.progress = random
+        setTimeout(() => self.setRandom(), 3000 )
+      }
+    },
+    mounted() {
+      // this.setRandom()
+    },
+  }
+</script>
+
+<style lang="stylus">
+  .progressbar
+    border: 1px solid green
+    width: 100%
+    background: #333
+
+    .inner
+      padding: .4em 
+      background: blue
+      transition: all 900ms ease-out
+</style>
