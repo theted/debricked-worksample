daaf1720f69157db51a6ddc034fb1788b93c5bd6
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-11-04 17:12:27 +0100

    Simple spinner component

---
+++ src/components/Spinner/Spinner.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  .spinner
+    .inner
+</template>
+
+<script>
+  export default {
+    name: 'spinner',
+    props: ['display']
+  }
+</script>
+
+<style lang="stylus" src="./Spinner.styl"></style>
