91396f87e82e4237d70cf5ec6d7de0505d27f2f9
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-11-04 17:14:23 +0100

    Starting implementation of Config component

---
+++ src/components/Config/Config.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .config(v-bind:class="{ active: expanded }")
+    a(href="#" @click="toggleActive") C
+
+    .wrap(v-bind:class="{ hidden: !expanded }")
+      .field
+        h4 Text size
+        // TODO: increase or decrease
+
+      .field
+        h4 Theme
+        // TODO: set dark or light theme, maybe background?
+        // TODO: maybe custom colorz?
+
+</template>
+
+<script>
+  export default {
+    name: 'config',
+    data() {
+      return {
+        expanded: false,
+      }
+    },
+    methods: {
+      toggleActive() { this.expanded = !this.expanded }
+    },
+    mounted() {
+    },
+  }
+</script>
+
+<style lang="stylus" src="./Config.styl"></style>
