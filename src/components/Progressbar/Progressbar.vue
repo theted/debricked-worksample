4f0be0a214b917bc257f1977b49a1dd4c1ac1383
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-11-04 16:38:58 +0100

    Add styling

--- src/components/Progressbar/Progressbar.vue
+++ src/components/Progressbar/Progressbar.vue
@@ -11,14 +11,4 @@
   }
 </script>
 
-<style lang="stylus">
-  .progressbar
-    border: 1px solid green
-    width: 100%
-    background: #333
-
-    .inner
-      padding: .4em 
-      background: blue
-      transition: all 1600ms ease-out
-</style>
+<style lang="stylus" src="./Progressbar.styl"></style>
