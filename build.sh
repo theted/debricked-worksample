4cd3d495781427ac791c04416a078074da7fd993
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-11-05 18:58:12 +0100

    Remove previously running container

--- build.sh
+++ build.sh
@@ -6,10 +6,14 @@ NAME="debricked-worksample"
 PORT="4244"
 
 # build the docker image
-echo "Building image..."
+echo "Building $NAME image..."
 docker build -t $NAME .
+
+# Stop previous running container
+echo "Stopping previous $NAME container..."
+docker stop $NAME
 
 # start up a docker container w/ image
 echo "Starting container..."
-docker run -d --rm -it -p $PORT:$PORT $NAME
-echo "...Running $NAME on port $PORT!"
+docker run -d --rm -it -p $PORT:$PORT --name $NAME $NAME
+echo "Running $NAME on port $PORT!"
