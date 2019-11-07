b3bfb1e9f2e29079e87edbcad0e7a8496feac3e4
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-11-07 05:05:17 +0100

    Conclude file uploads

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const cors = require('cors')
 const debricked = require('./models/debricked.js')
 const fileUpload = require('express-fileupload')
 const debug = require('debug')('app')
+const utils = require('./models/utils.js')
 const app = express()
 const config = { port: 4244 }
+
 require('dotenv').config()
 
 // set filesize limits
@@ -23,23 +25,26 @@ app.use(cors())
 // enable files upload
 app.use(fileUpload({ createParentPath: true }))
 
-
 // serve static files
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'src')))
 
 // receive file uploads in form of POST requests
-app.post('/upload', (req, res, next) => {
-  // Object.keys(req.body).forEach(key => console.log(key, '=>', req.body[key]))
+app.post('/upload', async (req, res, next) => {
 
+  // require files
   if (!req.files)
     return res.status(400).send('No files were uploaded.')
 
+  // get file name
   let file = req.files.data
+  let fullName = __dirname + '/uploads/' + file.name
+
+  // move the file 
+  // TODO; read JSON, set name of repository
   file.mv('./uploads/' + file.name)
 
-  // send response / debugger
-  console.log({
+  let output = {
     status: true,
     message: 'File is uploaded',
     data: {
@@ -47,9 +52,27 @@ app.post('/upload', (req, res, next) => {
       mimetype: file.mimetype,
       size: file.size
     }
-  })
+  }
 
-  res.send('Upload complete: ' + file.name)
+  // now that we have saved file locally, send it to Debricked API
+  let uploadResult = await debricked.uploadFile(fullName)
+
+  // conclude the upload
+  // TODO: add support for adding multiple files before conclude...
+  let concludeResult = await debricked.concludeUpload(uploadResult.ciUploadId)
+
+  // append result to output
+  output.ciUploadId = uploadResult.ciUploadId
+  output.uploadProgramsFileId = uploadResult.uploadProgramsFileId
+  output.concludeResult = concludeResult // <- temp
+
+  res.send(output)
+})
+
+// get status using debricked API...
+app.get('/status/:id', async (req, res, next) => {
+  let status = await debricked.status(req.params.id).catch(err => res.send(err))
+  res.send(status)
 })
 
 // serve index.html to all other GET requests
