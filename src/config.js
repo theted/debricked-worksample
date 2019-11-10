000216ddf1d3c9a0bb3648434af5c5d3ddc46f6f
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-11-11 00:18:46 +0100

    Fix misspelled endpoint

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const ENVIRONMENT = (window.location.hostname === 'localhost') ? 'development' : 'production'
 const ENDPOINT = (ENVIRONMENT === 'production') ? 'https://' + window.location.hostname : 'http://localhost:4244/'
 
-console.log('use endpoint:', endpoint)
+console.log('use endpoint:', ENDPOINT)
 
 const config = {
   endpoint: ENDPOINT
