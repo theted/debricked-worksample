20f19f3fd0c53a8080c1b5900fb42c7f1f89f999
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-11-11 00:19:59 +0100

    Add trailing /

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const ENVIRONMENT = (window.location.hostname === 'localhost') ? 'development' : 'production'
-const ENDPOINT = (ENVIRONMENT === 'production') ? 'https://' + window.location.hostname : 'http://localhost:4244/'
+const ENDPOINT = (ENVIRONMENT === 'production') ? 'https://' + window.location.hostname + '/' : 'http://localhost:4244/'
 
 console.log('use endpoint:', ENDPOINT)
 
